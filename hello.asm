a972a99028cfea58829cdcb175166a22f13f856f
taro0 <yutaro.ono.418@gmail.com>
2021-12-06 22:56:21 +0900

    fix: Normally exit

--- hello.asm
+++ hello.asm
@@ -5,8 +5,12 @@ message: db 'hello, world!', 10
 
 section .text
 _start:
-    mov rax, 1        ; system call number
+    mov rax, 1        ; write system call number
     mov rdi, 1        ; argument #1: descriptor to write (stdout)
     mov rsi, message  ; argument #2: first address of string
     mov rdx, 14       ; argument #3: the number of bytes to write
     syscall           ; invoke syscall
+
+    mov rax, 60       ; exit system call number
+    xor rdi, rdi
+    syscall
